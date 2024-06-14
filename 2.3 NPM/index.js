13a4d14aa77249a9ac72d8ee80f6bd1090723519
Chodipilli Ajay <115925992+chodipilliajay@users.noreply.github.com>
2024-06-14 22:01:56 +0530

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,12 @@
+
+
+// var generateName = require("sillyname");
+import generateName from "sillyname";
+var sillyname = generateName();
+
+console.log(`My name is ${sillyname}`);
+
+
+import {randomSuperhero} from "superheroes";
+console.log(`I am ${randomSuperhero()}`);
+//above 2 lines from the output may be the server errrs.
